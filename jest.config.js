c90763d4541f83cbf6f04ef45a93f9a69402462b
Tomer Ovadia <o.tomerr@gmail.com>
2021-02-02 08:27:57 +0200

    :sparkles: Add express API

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/react-app', '<rootDir>/libs/ui-header'],
+  projects: [
+    '<rootDir>/apps/react-app',
+    '<rootDir>/libs/ui-header',
+    '<rootDir>/apps/api',
+  ],
 };
